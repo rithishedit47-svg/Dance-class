a501d160e992191ad756915cf208c7f5f754973e
rithishedit47-svg <rithishedit47@gmail.com>
2025-12-18 10:24:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,13 +11,16 @@ const contactRoutes = require('./routers/contactRouters');
 const enrollmentRoutes = require('./routers/enrollmentRouters');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Connect to MongoDB
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://rad-chimera-3dc2a8.netlify.app'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
